629a7a802d45e57898a3f397bdd90f1151da7507
karan <karansachdev886@gmail.com>
2018-03-30 16:22:41 -0700

    tic tac toe grid

--- game.js
+++ game.js
@@ -1,36 +1,65 @@
-var clickId = 'o';
+var clickId = 'x';
+
 
 window.onload = function(){
-
-  createGrid(3,3,65,65,6,'#e5e6e8','black','blue');
+  var options1 = {
+    width:50,
+    height:50,
+    offset:5,
+    cellColor: 'white',
+    borderColor: 'black',
+    containerId: 'grid1'
+  }
+  var options2 = {
+    width:50,
+    height:50,
+    offset:5,
+    cellColor: 'white',
+    borderColor: 'blue',
+    containerId: 'grid2'
+  }
+  var options3 = {
+    width:50,
+    height:50,
+    offset:5,
+    cellColor: 'white',
+    borderColor: 'blue',
+    containerId: 'grid3'
+  }
+  createGrid(3,3,options1);
+  createGrid(3,3,options2);
+  createGrid(3,3,options3);
 }
 
-function createGrid(cols,rows,width,height,offset,cellColor,borderColor){
+function createGrid(cols,rows,options,margin){
 
   var grid = document.getElementById('grid-container');
   var color = 'black';
+  var container = document.createElement('div');
+  container.setAttribute('id',options.containerId);
 
   for(var i=0;i<cols;i++){
     for(var j=0;j<rows;j++){
       var button = document.createElement('button');
       button.col = i;
       button.row = j;
-      button.style.width = width + 'px';
-      button.style.height = height + 'px';
+      button.grid = options.containerId;
+      button.style.width = options.width + 'px';
+      button.style.height = options.height + 'px';
 
       button.style.borderStyle = 'solid';
-      button.style.borderWidth = offset + 'px';
+      button.style.borderWidth = options.offset + 'px';
       button.style.outline = '0';
 
-     button.setAttribute('class','grid-btn');
+      button.setAttribute('class','grid-btn');
 
-      if(cellColor){
-        button.style.backgroundColor = cellColor;
-        button.style.color = cellColor;
+      if(options.cellColor){
+        button.style.backgroundColor = options.cellColor;
+        button.style.color = options.cellColor;
       }
 
-      if(borderColor){
-        button.style.borderColor = borderColor;
+      if(options.borderColor){
+        button.style.borderColor = options.borderColor;
       }
 
       if(j == 0){
@@ -48,10 +77,14 @@ function createGrid(cols,rows,width,height,offset,cellColor,borderColor){
       }
       button.innerText = 'o';
       button.setAttribute('onclick','onButtonClick(this)');
-      grid.appendChild(button);
+      container.appendChild(button);
     }
-    grid.innerHTML += '<br/>'
+    container.innerHTML += '<br/>'
   }
+  //container.style.backgroundColor = options.borderColor;
+  //container.style.transform = 'rotateX('+options.perspective+'deg)';
+  grid.appendChild(container);
+  grid.innerHTML += '<br/>';
 }
 
 function onButtonClick(b){
